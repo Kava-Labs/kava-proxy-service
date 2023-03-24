19ea0691db6a16357a469bbf9b4625966d708092
Levi Schoen <levismschoen@gmail.com>
2023-03-24 14:11:17 -0700

    add /servicecheck endpoint to distinguish between the service being running and healthy

---
+++ service/handlers.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// createHealthcheckHandler creates a health check handler function that
+// will respond 200 ok if the proxy service is able to connect to
+// it's dependencies and functioning as expected
+func createHealthcheckHandler(service *ProxyService) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var combinedErrors error
+
+		service.Debug().Msg("/healthcheck called")
+
+		// check that the database is reachable
+		err := service.database.HealthCheck()
+
+		if err != nil {
+			errMsg := fmt.Errorf("proxy service unable to connect to database")
+			combinedErrors = errors.Join(combinedErrors, errMsg)
+		}
+
+		if combinedErrors != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			w.Write([]byte(combinedErrors.Error()))
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("proxy service is healthy"))
+	}
+}
+
+// createServicecheckHandler creates a service check handler function that
+// will respond 200 ok if the proxy service is running
+func createServicecheckHandler(service *ProxyService) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		service.Debug().Msg("/servicecheck called")
+
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("proxy service is in service"))
+	}
+}
