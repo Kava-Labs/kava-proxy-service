71fa94c30b1e62fd1b8f7a38f35d7d91256c38ef
drklee3 <github@dlee.dev>
2023-05-12 15:15:57 -0700

    fix: separate migrations for index additions (#28)
    * fix: separate migrations for index additions
    
    * Remove previous migration
    
    migrations shouldn't be modified but oops D:

--- clients/database/migrations/20230510135051_add_proxied_request_metrics_request_time_index.up.sql
+++ clients/database/migrations/20230510135051_add_proxied_request_metrics_request_time_index.up.sql
@@ -1,2 +1,1 @@
 CREATE INDEX CONCURRENTLY IF NOT EXISTS request_time_idx ON proxied_request_metrics(request_time);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS id_idx ON proxied_request_metrics(id);
---
+++ clients/database/migrations/20230512150351_add_proxied_request_metrics_id_index.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX CONCURRENTLY IF NOT EXISTS id_idx ON proxied_request_metrics(id);
