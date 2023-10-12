d2f4125c24ec26b856d2202cea47075bf01edd1a
Robert Pirtle <astropirtle@gmail.com>
2023-10-12 10:12:44 -0700

    rename e2e kava node to kavapruning

--- scripts/wait-for-kava-node-running.sh
+++ scripts/wait-for-kava-node-running.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -x
 
-until kava status -n tcp://localhost:"${KAVA_HOST_COSMOS_RPC_PORT}" | jq
-do
+until kava status -n tcp://localhost:"${KAVA_PRUNING_HOST_COSMOS_RPC_PORT}" | jq; do
     echo "waiting for kava service to be running"
     sleep 1
 done
 
-BLOCK_NUMBER=$(kava status -n tcp://localhost:"${KAVA_HOST_COSMOS_RPC_PORT}" | jq .sync_info.latest_block_height)
+BLOCK_NUMBER=$(kava status -n tcp://localhost:"${KAVA_PRUNING_HOST_COSMOS_RPC_PORT}" | jq .sync_info.latest_block_height)
 
 # seem to need 2 blocks for the evm api to be really "ready"
 # stopping at block 1 leads to 504 gateway timeouts when running
 # e2e tests after a full restart of all containers from zero state
-until [ "$BLOCK_NUMBER" != '"1"' ] && [ "$BLOCK_NUMBER" != '"0"' ] &&  [ "$BLOCK_NUMBER" != "" ]
-do
-    BLOCK_NUMBER=$(kava status -n tcp://localhost:"${KAVA_HOST_COSMOS_RPC_PORT}"| jq .sync_info.latest_block_height)
+until [ "$BLOCK_NUMBER" != '"1"' ] && [ "$BLOCK_NUMBER" != '"0"' ] && [ "$BLOCK_NUMBER" != "" ]; do
+    BLOCK_NUMBER=$(kava status -n tcp://localhost:"${KAVA_PRUNING_HOST_COSMOS_RPC_PORT}" | jq .sync_info.latest_block_height)
     echo "waiting for kava service to make a block"
     sleep 0.5
 done
