461c9fc30185d916f6758c4e1020a306e94fd2b5
Vladyslav Budichenko <boodyvo@gmail.com>
2024-09-12 16:29:01 -0400

    feat: test metrics for proxy passing env

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ package config
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/url"
 	"os"
 	"strconv"
@@ -346,6 +347,10 @@ func ReadConfig() Config {
 	// best effort to parse, callers are responsible for validating
 	// before using any values read
 	parsedHostnameToAccessControlAllowOriginValueMap, _ := ParseRawHostnameToHeaderValueMap(rawHostnameToAccessControlAllowOriginValueMap)
+
+	kk, ok := os.LookupEnv(METRIC_DATABASE_ENABLED_ENVIRONMENT_KEY)
+	log.Println("PARSING METRIC_DATABASE_ENABLED:", kk, ok)
+	log.Println("PARSED METRIC_DATABASE_ENABLED:", EnvOrDefaultBool(METRIC_DATABASE_ENABLED_ENVIRONMENT_KEY, DEFAULT_METRIC_DATABASE_ENABLED))
 
 	return Config{
 		ProxyServicePort:                              os.Getenv(PROXY_SERVICE_PORT_ENVIRONMENT_KEY),
